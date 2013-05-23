9988b41926cfc430f485b4bbd2112abc5ad34901
Simon Johansson <870805@gmail.com>
2013-05-23 23:51:24 +0100

    Update metadata to reflect the rest of the repo.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             "rundeck"
 maintainer       "Simon Johansson"
 maintainer_email "simon@simonjohansson.com"
-license          "All rights reserved"
-description      "Installs/Configures chef-rundeck"
+license          "MIT"
+description      "Installs/Configures rundeck"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
