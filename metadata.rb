f71f7bffa5ec8d0017ff7934a4fc1b5e440eb703
Simon Johansson <870805@gmail.com>
2013-05-24 00:23:15 +0100

    Add supports to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,9 @@ maintainer_email "simon@simonjohansson.com"
 license          "MIT"
 description      "Installs/Configures rundeck"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+supports         "ubuntu", ">= 12.04"
 version          "0.1.0"
+
 
 depends "java", "= 1.11.4"
 depends "database", "= 1.3.12"
