3ee7ecedca11a410d9c707102d3e7bacb43cc045
Simon Johansson <870805@gmail.com>
2013-05-23 22:54:22 +0100

    Default recipee, download, install, enable and start rundeck

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,6 @@
+default['rundeck']['version'] = '1.5.2-1-GA'
+default['rundeck']['deb_url'] = "http://download.rundeck.org/deb/rundeck-#{node['rundeck']['version']}.deb"
+
 default['rundeck']['mysql']['host'] = 'localhost'
 default['rundeck']['mysql']['user'] = 'rundeck'
 default['rundeck']['mysql']['password'] = 's3cr3t'
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,3 +8,20 @@
 #
 include_recipe "apt"
 include_recipe "java::openjdk"
+
+# Download the rundeck deb
+remote_file "#{Chef::Config['file_cache_path']}/rundeck-#{node['rundeck']['version']}.deb" do
+  source node['rundeck']['deb_url']
+  action :create_if_missing
+end
+
+# Install it
+dpkg_package "#{Chef::Config['file_cache_path']}/rundeck-#{node['rundeck']['version']}.deb" do
+  action :install
+end
+
+service 'rundeckd' do
+  provider Chef::Provider::Service::Upstart if platform?('ubuntu') && node['platform_version'].to_f >= 12.04
+  supports :status => true, :restart => true
+  action [ :enable, :start ]
+end
