f8eaf492cf21e2d348a9aea852ec2686fe6ffb1a
Simon Johansson <simon@editd.com>
2013-05-23 17:57:17 +0100

    Add dependency for database and mysql

--- Berksfile
+++ Berksfile
@@ -1,5 +1,6 @@
 site :opscode
 
 cookbook "java", "= 1.11.4"
-
+cookbook "database", "= 1.3.12"
+cookbook "mysql", "= 3.0.0"
 metadata
--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
 depends "java", "= 1.11.4"
+depends "database", "= 1.3.12"
+depends "mysql", "= 3.0.0"
