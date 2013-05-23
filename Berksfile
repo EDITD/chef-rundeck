590e62cc659475aae5c05f4ba75cb2b3ec9a8fbe
Simon Johansson <simon@editd.com>
2013-05-23 17:07:51 +0100

    Why, rundeck needs java, lets start with that

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 site :opscode
 
+cookbook "java", "= 1.11.4"
+
 metadata
--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          "All rights reserved"
 description      "Installs/Configures chef-rundeck"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+
+depends "java", "= 1.11.4"
