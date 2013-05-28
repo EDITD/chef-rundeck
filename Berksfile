9535e4036778ef0d237825b2d443b576034ddfc0
Simon Johansson <simon@editd.com>
2013-05-28 14:41:57 +0100

    Updated deps in berksfile and metadata.

--- Berksfile
+++ Berksfile
@@ -1,8 +1,8 @@
 site :opscode
 
-cookbook "java", "= 1.11.4"
-cookbook "database", "= 1.3.12"
-cookbook "mysql", "= 3.0.0"
-cookbook "apt", "= 1.9.2"
-cookbook "nginx", "= 1.6.0"
+cookbook "java", "> 1.10.2"
+cookbook "database", "> 1.3.12"
+cookbook "mysql", "> 2.1.2"
+cookbook "apt", "> 1.9.2"
+cookbook "nginx", "> 0.100.2"
 metadata
--- metadata.rb
+++ metadata.rb
@@ -8,8 +8,8 @@ supports         "ubuntu", ">= 12.04"
 version          "0.1.0"
 
 
-depends "java", "= 1.11.4"
-depends "database", "= 1.3.12"
-depends "mysql", "= 3.0.0"
-depends "apt", "= 1.9.2"
-depends "nginx", "= 1.6.0"
+depends "java", ">= 1.10.2"
+depends "database", ">= 1.3.12"
+depends "mysql", ">= 2.1.2"
+depends "apt", ">= 1.9.2"
+depends "nginx", ">= 0.100.2"
